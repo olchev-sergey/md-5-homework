56fd1e1c020e3b0bda896bf19e3f5fd00f9be0e2
Olchev Sergey <sergey200012111sergey@gmail.com>
2024-02-15 18:53:59 +0300

    add api and clean code

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,37 @@
-const CARDS = [
-  {
-    id: '1',
-    name: 'Mama\'s Kitchen',
-    description: 'American, 4.7 stars',
-    rating: 4,
-    url: '/mamas-kitchen.png',
-  },
-  {
-    id: '2',
-    name: 'The Burger Joint',
-    description: 'American, 4.6 stars',
-    rating: 4,
-    url: '/burger-joint.png',
-  },
-  {
-    id: '3',
-    name: 'Pasta Express',
-    description: 'American, 4.5 stars',
-    rating: 4,
-    url: '/pasta-express.png',
-  },
-  {
-    id: '4',
-    name: 'Taco Fiesta',
-    description: 'American, 4.8 stars',
-    rating: 4,
-    url: '/taco-fiesta.png',
-  },
-]
+import { useGetRestaurants } from './api'
+import StarIcon from './assets/star.svg?react'
+import LogoIcon from './assets/stair.svg?react'
 
 function App() {
+  const { data: restaurants = [] } = useGetRestaurants()
+
   return (
     <>
       <header>
         <div>
-          <img alt="logo" src="/stair.svg" width="16"/>
+          <LogoIcon width={16} height={16} />
           <span>Eats</span>
         </div>
         <div>
-          <img alt="profile" src="/avatar.png" width="24"/>
+          <img alt="profile" src="/avatar.png" width="24" />
         </div>
       </header>
       <main>
-        <input placeholder="Search for restaurants"/>
+        <input placeholder="Search for restaurants" />
         <section>
-          {CARDS.map(({id, description, name, url}) => (
+          {restaurants.map(({ id, description, name, url }) => (
             <div key={id}>
-              <img src={url} width={160}/>
+              <img src={url} width={160} />
               <div>
                 <p>{name}</p>
                 <p>{description}</p>
               </div>
               <div>
-                <img alt="star" src="/star.svg" width="16"/>
-                <img alt="star" src="/star.svg" width="16"/>
-                <img alt="star" src="/star.svg" width="16"/>
-                <img alt="star" src="/star.svg" width="16"/>
-                <img alt="star" src="/star.svg" width="16"/>
+                <StarIcon width={16} height={16} />
+                <StarIcon width={16} height={16} />
+                <StarIcon width={16} height={16} />
+                <StarIcon width={16} height={16} />
+                <StarIcon width={16} height={16} />
               </div>
             </div>
           ))}
